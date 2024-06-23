11443c7282b7b10c86abc3cbc56f5ac3fb026571
Priya Mishra <mishrapriya549@gmail.com>
2024-06-23 13:40:23 +0530

    Initial commit

---
+++ Day 1/13_InverseAlphabetPattern.java
@@ -0,0 +1,12 @@
+class InverseAlphabetPattern {
+    public static void main(String args[]) {
+        int n = 5;
+
+        for (int i = n; i >= 1; i--) {
+            for (char j = 'A'; j < 'A' + i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Day 1/1_SquarePattern.java
@@ -0,0 +1,13 @@
+class SquarePattern{
+    public static void main(String[] args){
+        int row=5;
+        int column=5;
+
+        for(int i=0;i<row;i++){
+            for(int j=0;j<column;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
